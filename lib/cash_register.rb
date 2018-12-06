f648efbd3f6627ab5c4ac8d48729d72c57730281
Jon Mackey <jon@jonmackey.party>
2018-12-06 20:28:50 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,36 @@
+class CashRegister
+  attr_accessor(:total, :discount, :items, :running_total)
 
+  
+  def initialize(discount = nil)
+    @total = 0
+    @discount = discount
+    @items = [ ]
+    @running_total = [ ]
+  end
+  
+  def total
+    @total
+  end
+  
+  def add_item(title, price, quantity = 1)
+    @total = @total + price*quantity
+    @items << [title] * quantity
+    @items.flatten!
+    @running_total << price*quantity
+  end
+  
+  def apply_discount
+  
+  
+  end
+  
+  def void_last_transaction
+    @total = @total - @running_total.last
+  end
+  
+  def items
+    @items
+  end
+  
+end
