dbfc722c554db223b349bccb197c80d394564ccc
Jon Mackey <jon@jonmackey.party>
2018-12-06 19:43:22 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,24 @@
+class CashRegister
+  attr_accessor(:total, :discount, :items)
 
+  
+  def initialize(discount = nil)
+    @total = 0
+    @discount = discount
+    @items = [ ]
+  end
+  
+  def total
+    self.total
+  end
+  
+  def add_item(title, price, quantity = 1)
+    self.total = self.total + price*quantity
+    self.items << title
+  end
+  
+  
+  def items
+    self.items
+  end
+end
